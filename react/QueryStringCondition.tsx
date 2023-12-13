d5766c4308613267ae2b12ee4dc8821870658417
Tiago de Andrade Freire <freire.tiago@gmail.com>
2023-12-13 10:02:25 -0300

    chore: debugging QueryStringCondition

--- react/QueryStringCondition.tsx
+++ react/QueryStringCondition.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useRuntime } from 'vtex.render-runtime'
 
-
-type Props =  {
+type Props = {
   parameter: string
   value: string
   Then?: React.FC<unknown>
@@ -12,6 +11,12 @@ type Props =  {
 const QueryStringCondition = (props: Props) => {
   const { parameter, value, Then, Else } = props
   const { query } = useRuntime()
+
+  // if (!canUseDOM) {
+  //   return null
+  // }
+
+  console.log(JSON.stringify({ query, parameter, value, "query?.[parameter]": query?.[parameter] ?? "nada", "query?.[parameter] === value": query?.[parameter] === value }, null, 2))
 
   if (parameter && value && query?.[parameter] === value) {
     return !!Then && <Then />
